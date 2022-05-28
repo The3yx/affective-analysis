7611b7d0cd1933f6399fe3e5e016be648a3a06fd
胡鹏 <72617488+weikeduo1@users.noreply.github.com>
2022-05-28 17:14:24 +0800

    Update LSTMmask.py

--- LSTMmask.py
+++ LSTMmask.py
@@ -7,12 +7,19 @@ from torch.utils.data import Dataset
 from torch.utils.tensorboard import SummaryWriter
 import ssl
 import torch.nn.functional as F
+import os
 ssl._create_default_https_context = ssl._create_unverified_context
 
 class MyDataset(Dataset):    #继承Datasets
-    def __init__(self,target,path,maxLen):    # 初始化一些用到的参数，一般不仅有self
-        self.target = target
-        self.path=path
+    def __init__(self,path,sentiment,maxLen):    # 初始化一些用到的参数，一般不仅有self
+        target=[]
+        path1=[]
+        for i in os.listdir(path):
+            path1.append(os.path.join(path,i))
+            num=(int)(i.strip(".npy"))
+            target.append(sentiment[num]-1)
+        self.target=target
+        self.path=path1
         self.maxLen=maxLen
     def __len__(self):    # 数据集的长度
         return len(self.target)
@@ -23,23 +30,17 @@ class MyDataset(Dataset):    #继承Datasets
         return mask
     def __getitem__(self, idx):    # 按照索引读取每个元素的具体内容
         target = self.target[idx]
-        x = np.load(".//data1//" + self.path[idx] + '.npy', allow_pickle=True)
+        x = np.load(self.path[idx], allow_pickle=True)
         mask=self.getMask(x)
         if x.shape[0]<self.maxLen:
             x=np.concatenate([x, np.zeros((self.maxLen-x.shape[0],(int)(x.shape[1])),'f')], axis=0)
         return x ,target,mask
 device = 'cuda'
-total = pd.read_csv(r'C:\Users\86131\Desktop\NAS-master\save2.csv')
-name = total['name']
-lenth = len(name)
-flag=np.load("../now/flag.npy")
-dataLen=len(flag)
-maxLen=3411
-maxLen1=170
-# c_feature=np.mean(c_feature,0)
-# c_feature=torch.reshape(torch.tensor(c_feature),(1,-1))
-trainData=MyDataset(flag[0:(int)(dataLen*0.8)],name[0:(int)(dataLen*0.8)],maxLen1)
-testData=MyDataset(flag[(int)(dataLen*0.8):(int)(dataLen)],name[(int)(dataLen*0.8):(int)(dataLen)],maxLen1)
+maxLen=77 #按照之前设置求的maxlen进行设置进行分类
+total = pd.read_csv(r'C:\Users\86131\Desktop\twitter-sentiment-analysis-self-driving-cars\train.csv')
+sentiment = total['sentiment']
+trainData=MyDataset("masktextTrainData",sentiment,maxLen)
+testData=MyDataset("masktextValData",sentiment,maxLen)
 
 trainLen=len(trainData)
 testLen=len(testData)
@@ -75,10 +76,10 @@ class LSTM(nn.Module):
         out = self.decoder(attX)  # [B, label]
         return out
 
-input_dim = 512
-hidden_dim = 30
+input_dim = 768 #按照之前的维度进行设置
+hidden_dim = 20
 num_layers = 1
-output_dim = 4
+output_dim = 5
 
 newModel =LSTM(input_dim=input_dim, hidden_dim=hidden_dim, output_dim=output_dim, num_layers=num_layers)
 newModel=newModel.to(device)
@@ -123,7 +124,7 @@ for i in range(epoch):
         newLoss+=loss
         acc+=(output.argmax(1)==flag).sum()
         del wav, flag, mask, loss
-    print(i)
+    print(str(i) + "   loss:" + str(newLoss) + "   acc:" + str(acc / testLen))
     write.add_scalar("loss",newLoss,theStep)
     write.add_scalar("acc",acc/testLen,theStep)
 write.close()
