b0edea941070c4e0fde72625658f2de26a42f0ff
胡鹏 <72617488+weikeduo1@users.noreply.github.com>
2022-05-28 16:23:18 +0800

    Add files via upload

---
+++ BERT-BASE.py
@@ -0,0 +1,59 @@
+import torch
+from pytorch_pretrained_bert import BertTokenizer, BertModel
+import soundfile as sf
+import torch
+import librosa
+import numpy as np
+from fairseq.models.wav2vec import Wav2VecModel #fairseq.__version__ = 0.10.1
+import pandas as pd
+import ssl
+from torch import nn
+ssl._create_default_https_context = ssl._create_unverified_context
+
+tokenizer = BertTokenizer.from_pretrained(r'C:\Users\86131\PycharmProjects\torch1\bert-base-chinese', do_lower_case=True)
+bert = BertModel.from_pretrained(r'C:\Users\86131\PycharmProjects\torch1\bert-base-chinese')
+bert.eval()
+
+# text = '我爱北京天安门。'
+# text = tokenizer.tokenize(text)
+# print(text)  # ['我', '爱', '北', '京', '天', '安', '门', '。']
+# text_id = tokenizer.convert_tokens_to_ids(text)
+# print(text_id)  # [2769, 4263, 1266, 776, 1921, 2128, 7305, 511]
+# text_id = torch.tensor(text_id, dtype=torch.long)
+# text_id = text_id.unsqueeze(dim=0)
+# print(text_id)  # tensor([[2769, 4263, 1266,  776, 1921, 2128, 7305,  511]])
+# output = bert(text_id)[0]
+# print(len(output))  # 12层
+# text_embedding = bert(text_id)[0][0]  # 取第1层，也可以取别的层。
+# text_embedding = text_embedding.detach()  # 切断反向传播。
+# print(text_embedding.shape)  # torch.Size([1, 8, 768])
+
+
+device = 'cuda'
+total = pd.read_csv(r'C:\Users\86131\Desktop\twitter-sentiment-analysis-self-driving-cars\train.csv')
+sentiment = total['sentiment']
+texts = total['text']
+lenth = len(texts)
+h=0
+for i in range(lenth):
+    # target = torch.tensor(int(sentiment[i])).reshape(1)
+    text=texts[i]
+    with torch.no_grad():
+        text = tokenizer.tokenize(text)
+        text_id = tokenizer.convert_tokens_to_ids(text)
+        text_id = torch.tensor(text_id, dtype=torch.long)
+        text_id = text_id.unsqueeze(dim=0)
+        output = bert(text_id)[0]
+        text_embedding = bert(text_id)[0][0]  # 取第1层，也可以取别的层。
+        text_embedding = text_embedding.detach().squeeze().t().cpu().numpy()  # 切断反向传播。# torch.Size([1, 8, 768])
+        text_embedding=text_embedding.reshape(-1,768)
+        feature = np.mean(text_embedding, 0)
+        if h==4:
+            h=0
+            np.save("./textValData/{}.npy".format(i), feature, allow_pickle=True)
+        else:
+            h+=1
+            np.save("./textTrainData/{}.npy".format(i), feature, allow_pickle=True)
+        # np.save("./textValData/f{}.npy".format(i),target, allow_pickle=True)
+        # flag.append(target.numpy())
+        # z1.append(z_feature)
---
+++ LSTM.py
@@ -0,0 +1,119 @@
+import torch
+import numpy as np
+from torch import nn
+from torch.utils.data import DataLoader
+from torch.utils.data import Dataset
+from torch.utils.tensorboard import SummaryWriter
+import ssl
+import os
+ssl._create_default_https_context = ssl._create_unverified_context
+
+class MyDataset(Dataset):    #继承Datasets
+    def __init__(self,path):    # 初始化一些用到的参数，一般不仅有self
+        target=[]
+        path1=[]
+        for i in os.listdir(path):
+            tdir=os.path.join(path,i)
+            tLen=len(os.listdir(tdir))
+            for j in range(tLen):
+                target.append(int(i))
+            for j in os.listdir(tdir):
+                path1.append(os.path.join(tdir,j))
+        self.target=target
+        self.path=path1
+    def __len__(self):    # 数据集的长度
+        return len(self.target)
+    def __getitem__(self, idx):    # 按照索引读取每个元素的具体内容
+        target = self.target[idx]
+        x = np.load(self.path[idx], allow_pickle=True)
+        return x ,target
+device = 'cuda'
+path=r"C:\Users\86131\Desktop\data\wav2vecOut"
+batch_size=128
+
+trainData=MyDataset(os.path.join(path,"train"))
+testData=MyDataset(os.path.join(path,"val"))
+
+trainLen=len(trainData)
+testLen=len(testData)
+print(trainLen)
+print(testLen)
+trainDataLoader=DataLoader(trainData,batch_size=batch_size,shuffle=True,drop_last=True)
+testDataLoader=DataLoader(testData,batch_size=batch_size,shuffle=True,drop_last=True)
+
+
+class LSTM(nn.Module):
+    def __init__(self, input_dim, hidden_dim, num_layers, output_dim):
+        super(LSTM, self).__init__()
+        self.hidden_dim = hidden_dim
+        self.num_layers = num_layers
+        self.lstm = nn.LSTM(input_dim, hidden_dim, num_layers, batch_first=True)
+        self.decoder=nn.Sequential(
+            nn.Linear(hidden_dim,output_dim),
+            nn.Sigmoid()
+        )
+
+    def forward(self, x):
+        h0 = torch.zeros(self.num_layers, x.size(0), self.hidden_dim).requires_grad_().to(device)
+        c0 = torch.zeros(self.num_layers, x.size(0), self.hidden_dim).requires_grad_().to(device)
+        out, (hn, cn) = self.lstm(x, (h0.detach(), c0.detach()))
+        out.to(device)
+        return self.decoder(out)
+
+input_dim = 512
+hidden_dim = 30
+num_layers = 1
+output_dim = 2
+
+newModel =LSTM(input_dim=input_dim, hidden_dim=hidden_dim, output_dim=output_dim, num_layers=num_layers)
+newModel=newModel.to(device)
+
+theLoss=nn.CrossEntropyLoss()
+theLoss=theLoss.to(device)
+
+lr=1e-3
+optim =torch.optim.Adam(newModel.parameters(), lr=lr)
+
+epoch=100
+write =SummaryWriter("./log")
+theStep=0
+for i in range(epoch):
+    if i%10==0:
+        lr = lr*0.7
+        optim = torch.optim.Adam(newModel.parameters(), lr=lr)
+    newLoss=0
+    acc=0
+    theStep+=1
+    newModel.train()
+    for data in trainDataLoader:
+        wav,flag=data
+        wav=wav.reshape(-1,1,512)
+        flag=flag.reshape(-1)
+        wav=wav.to(device)
+        flag=flag.to(device)
+        output=newModel(wav)
+        output=output.reshape(batch_size,-1)
+        loss=theLoss(output,flag)
+        acc += (output.argmax(1) == flag).sum().detach().squeeze().t().cpu().numpy()
+        optim.zero_grad()
+        loss.backward()
+        optim.step()
+        del wav,flag,loss
+    newModel.eval()
+    for data in testDataLoader:
+        with torch.no_grad():
+            wav,flag=data
+            flag=flag.reshape(-1)
+            wav = wav.reshape(-1, 1, 512)
+            flag=torch.tensor(flag)
+            wav=wav.to(device)
+            flag=flag.to(device)
+            output=newModel(wav)
+            output = output.reshape(batch_size, -1)
+            loss=theLoss(output,flag)
+            newLoss+=loss
+            acc+=(output.argmax(1)==flag).sum()
+    print(str(i) + "   loss:" + str(newLoss) + "   acc:" + str(acc / testLen))
+    write.add_scalar("loss",newLoss,theStep)
+    write.add_scalar("acc",acc/testLen,theStep)
+write.close()
