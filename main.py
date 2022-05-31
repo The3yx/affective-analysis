6699254463f032e571218807a527f255ffbe34ef
The3yx <1340175409@qq.com>
2022-05-31 20:03:37 +0800

    nightly_update

---
+++ main.py
@@ -0,0 +1,55 @@
+import parser
+def TODO():
+    return
+
+
+def main(args):
+    TODO()
+    #加载数据集
+
+
+
+
+
+
+    #初始化MODEL
+
+
+
+
+
+
+    #训练
+
+
+
+
+
+
+
+
+    #Test
+    
+    return
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+
+    #加载超参数
+
+
+
+
+
+    args = 1
+    main(args)
