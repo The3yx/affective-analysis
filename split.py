80c5691f07e600cfcc6523ae0b24eb5771407ff4
zertow <1181527496@qq.com>
2022-06-10 00:04:10 +0800

    add split

---
+++ split.py
@@ -0,0 +1,10 @@
+import numpy as np
+import pandas as pd
+import random
+random.seed(1)
+sentiment = pd.read_csv("./dataset/train_pre.csv")
+eval_set = sentiment.sample(frac=0.2)
+eval_set.to_csv('./dataset/eval_split.csv', index=False)
+sentiment = sentiment.append(eval_set)
+sentiment=sentiment.drop_duplicates(subset=['sentiment','text'],keep=False)
+sentiment.to_csv('./dataset/train_split.csv', index=False)
